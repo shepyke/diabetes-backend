26629ced4c8eb1dfcf3d9f9bb32dd8b61987d1db
Mate Pasztor <mate.pasztor@gravityrd.com>
2018-01-05 16:04:30 +0100

    basic backend stuff for login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'diabetes'
+});
+
+router.post('/', function(req, res, next) {
+  var username = req.body.username;
+  var password= req.body.password;
+
+  connection.query(
+      "SELECT * FROM users WHERE username = ? AND password = ?",
+      [username, password], function(err, row, field){
+        if(err){
+          console.log(err);
+          res.send({ 'success': false, 'message': 'Could not connect to database'})
+        }
+
+        if(row.length > 0){
+          res.send({ 'success': true, 'user': row[0].username});
+        }else{
+          res.send({ 'success': false, 'message': 'User not found'});
+        }
+      }
+  );
+
 });
 
 module.exports = router;
