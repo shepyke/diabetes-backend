cfa856a5150123cc9881b8c579e74faf1d649ca8
Mate Pasztor <mate.pasztor@gravityrd.com>
2018-04-22 15:29:25 +0200

    calculate total income of nutritions and save all values to DB

--- routes/intakes.js
+++ routes/intakes.js
@@ -42,19 +42,23 @@ router.post('/addIntake', function(req, res, next) {
     var amount = intake.amount;
     var time = intake.time;
 
+    var food = req.body.food;
+    var glycemicIndex = food.glycemicIndex;
+    var totalCarbohydrate = (food.carbohydrate * amount);
+    var totalFat = (food.fat * amount);
+    var totalCalorie = (food.calorie * amount);
+    var totalProtein = (food.protein * amount);
+
     if(amount === '' || time === ''){
         res.send({ 'success': false, 'message': 'Please fill all mandatory field'});
     }else {
         connection.query(
-        "INSERT INTO intakes" +
-            "(`userId`, `foodId`, `amount`, `time`)" +
-            "VALUES (?, ?, ?, ?);",
-            [userId, foodId, amount, time],
+            "INSERT INTO intakes" +
+            "(`userId`, `foodId`, `amount`, `time`, `glycemicIndex`, `totalCarbs`, `totalFat`, `totalCalorie`, `totalProtein`) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);",
+            [userId, foodId, amount, time, glycemicIndex, totalCarbohydrate, totalFat, totalCalorie, totalProtein],
             function (err, row, field) {
-                if (err && err.toString().indexOf('username_UNIQUE') !== -1) {
-                    console.log(err);
-                    res.send({'success': false, 'message': 'This username is already in use. Please select another one.'})
-                }else if(err){
+                if(err){
                     console.log(err);
                     res.send({'success': false, 'message': 'Could not connect to database'})
                 }else{
