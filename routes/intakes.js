7167f2e70940d23c2c87ac6a7f3ca6573731936e
Mate Pasztor <mate.pasztor@gravityrd.com>
2018-04-26 01:12:58 +0200

    change Intakes backend to handle multivalued parameters and save only the total metrics to DB

--- routes/intakes.js
+++ routes/intakes.js
@@ -12,6 +12,8 @@ var connection = mysql.createConnection({
     timezone: 'utc'
 });
 
+var foods;
+
 router.get('/foods', function(req, res, next) {
 
     connection.query(
@@ -23,9 +25,8 @@ router.get('/foods', function(req, res, next) {
                 res.send({ 'success': false, 'message': 'Could not connect to database'})
             }
 
-            console.log(row);
-
             if(row.length > 0){
+                foods = row;
                 res.send({ 'success': true, 'foods': row});
             }else{
                 res.send({ 'success': false, 'message': 'There werent any food in the catalog'});
@@ -36,38 +37,53 @@ router.get('/foods', function(req, res, next) {
 });
 
 router.post('/addIntake', function(req, res, next) {
-    var intake = req.body.intake;
-    var userId = intake.userId;
-    var foodId = intake.foodId;
-    var amount = intake.amount;
-    var time = intake.time;
+    var intakes = req.body.intakes;
+    var userId = req.body.userId;
+    var time = req.body.time;
 
-    var food = req.body.food;
-    var glycemicIndex = food.glycemicIndex;
-    var totalCarbohydrate = (food.carbohydrate * amount);
-    var totalFat = (food.fat * amount);
-    var totalCalorie = (food.calorie * amount);
-    var totalProtein = (food.protein * amount);
+    var totalIntake = {
+        avgGI: 0,
+        totalCarbohydrate: 0,
+        totalFat: 0,
+        totalCalorie: 0,
+        totalProtein: 0,
+    };
 
-    if(amount === '' || time === ''){
-        res.send({ 'success': false, 'message': 'Please fill all mandatory field'});
-    }else {
+    var finalFoodIds = '';
+    var finalAmounts = '';
+
+    if (intakes.length > 0) {
+        for (var item of intakes) {
+            totalIntake.avgGI += foods[item.foodId - 1].glycemicIndex;
+            totalIntake.totalCarbohydrate += (foods[item.foodId - 1].carbohydrate * item.amount) / 100;
+            totalIntake.totalFat += (foods[item.foodId - 1].fat * item.amount) / 100;
+            totalIntake.totalCalorie += (foods[item.foodId - 1].calorie * item.amount) / 100;
+            totalIntake.totalProtein += (foods[item.foodId - 1].protein * item.amount) / 100;
+            finalFoodIds += item.foodId + ',';
+            finalAmounts += item.amount + ',';
+        }
+        totalIntake.avgGI = totalIntake.avgGI / intakes.length;
+
+        console.log("totalIntake: " + JSON.stringify(totalIntake,null,4));
+
         connection.query(
             "INSERT INTO intakes" +
-            "(`userId`, `foodId`, `amount`, `time`, `glycemicIndex`, `totalCarbs`, `totalFat`, `totalCalorie`, `totalProtein`) " +
+            "(`userId`, `foodId`, `amount`, `time`, `glycemicIndex`, `totalCarbs`, " +
+                "`totalFat`, `totalCalorie`, `totalProtein`) " +
             "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);",
-            [userId, foodId, amount, time, glycemicIndex, totalCarbohydrate, totalFat, totalCalorie, totalProtein],
+            [userId, finalFoodIds, finalAmounts, time, totalIntake.avgGI,
+                totalIntake.totalCarbohydrate, totalIntake.totalFat,
+                totalIntake.totalCalorie, totalIntake.totalProtein],
             function (err, row, field) {
-                if(err){
+                if (err) {
                     console.log(err);
                     res.send({'success': false, 'message': 'Could not connect to database'})
-                }else{
-                    res.send({'success': true, 'intake': intake});
+                } else {
+                    res.send({'success': true, 'intake': totalIntake});
                 }
             }
         );
     }
-
 });
 
 module.exports = router;
